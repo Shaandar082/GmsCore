d4d77a900aac4ded6031ad76c43709d03475fa33
Shaandar082 <157476351+Shaandar082@users.noreply.github.com>
2024-04-09 21:35:11 +0200

    Update gradlew.bat

--- gradlew.bat
+++ gradlew.bat
@@ -2,9 +2,9 @@
 @rem
 @rem Copyright 2015 the original author or authors.
 @rem
-@rem Licensed under the Apache License, Version 2.0 (the "License");
+@rem Licensed under the Apache License, Vrsion 2.0 (the "License");
 @rem you may not use this file except in compliance with the License.
-@rem You may obtain a copy of the License at
+@rem You may obtain a copy of the License a
 @rem
 @rem      https://www.apache.org/licenses/LICENSE-2.0
 @rem
